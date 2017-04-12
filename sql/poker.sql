18f881feb7a9e81e2aac9782a00b88106f4d176b
ss <ss>
2017-04-12 11:43:43 +0700

    d

--- sql/poker.sql
+++ sql/poker.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-04-07 12:53:58
+-- Generation Time: 2017-04-12 06:42:54
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -35,6 +35,10 @@ SET total = (SELECT COUNT(*) FROM  `caribbean_poker_income`);
 SELECT * FROM (
 SELECT count(*) AS style_count ,total, ROUND(count(*)/total *100,3) AS pr, player_card_type FROM `caribbean_poker_income` GROUP BY `player_card_type`)
 a ORDER BY pr ;
+
+SELECT * FROM (
+SELECT count(*) AS style_count ,total, ROUND(count(*)/total *100,3) AS pr, banker_card_type FROM `caribbean_poker_income` GROUP BY `banker_card_type`)
+a ORDER BY pr ;
 END$$
 
 DROP PROCEDURE IF EXISTS `win`$$
@@ -62,21 +66,22 @@ CREATE TEMPORARY TABLE temp1 (
      bet_lose_rate double,
      total_lose_rate double
    ) ENGINE=MEMORY;
+   
     WHILE i > 0 DO
-    truncate temp1;
+       /*truncate temp1;
     	INSERT INTO temp1(winlose, bet, `double`)
-    	SELECT  winlose ,bet, `double` FROM `caribbean_poker_income` ORDER BY RAND() ;
+    	SELECT  winlose ,bet, `double` FROM `caribbean_poker_income`  ;*/
     	SET i = i - 1;
         
     
-    SET winlose_total = (SELECT SUM(winlose) FROM temp1);
+    SET winlose_total = (SELECT SUM(winlose) FROM `caribbean_poker_income`);
     
-    SET bet_total = (SELECT SUM(bet) FROM temp1);
-    SET double_total = (SELECT SUM(`double`) FROM temp1);
+    SET bet_total = (SELECT SUM(bet) FROM `caribbean_poker_income`);
+    SET double_total = (SELECT SUM(`double`) FROM `caribbean_poker_income`);
     INSERT INTO temp2 (winlose_total, bet_total,  bet_lose_rate, total_lose_rate,  double_total) VALUES(winlose_total, bet_total, winlose_total/bet_total, winlose_total/(bet_total+double_total),   double_total);
     END WHILE;
 	SELECT *  FROM temp2 AS t ;
-    
+   SELECT sample ;
 END$$
 
 DELIMITER ;
@@ -94,18 +99,33 @@ CREATE TABLE IF NOT EXISTS `caribbean_poker_income` (
   `banker_card_type` varchar(20) NOT NULL,
   `banker_hand_card` varchar(255) NOT NULL,
   `banker_card_style` varchar(30) NOT NULL,
-  `banker_card_point` int(11) NOT NULL,
+  `banker_card_point` bigint(21) NOT NULL,
   `player_hand_card` varchar(255) NOT NULL,
   `player_card_style` varchar(30) NOT NULL,
-  `player_card_point` int(11) NOT NULL,
+  `player_card_point` bigint(21) NOT NULL,
   `winner` enum('banker','player','tie') NOT NULL,
   `odds` int(11) NOT NULL DEFAULT '1',
   `bet` int(11) NOT NULL,
   `double` int(11) NOT NULL DEFAULT '0',
   `winlose` int(11) NOT NULL,
-  `play_point` int(11) NOT NULL DEFAULT '1000000',
+  `play_point` bigint(21) NOT NULL DEFAULT '1000000',
+  `version` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=65296 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1431398 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `probability_table`
+--
+
+DROP TABLE IF EXISTS `probability_table`;
+CREATE TABLE IF NOT EXISTS `probability_table` (
+  `card_style` varchar(2) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `card_style` (`card_style`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=66665 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
